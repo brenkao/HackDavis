9bef478300e8b8b9d66f275481719bedc701667e
Michael Tang <miytang@ucsc.edu>
2016-05-08 00:52:09 -0700

    Somehwat done login

--- app/src/main/java/com/example/bren/hackdavis/MainActivity.java
+++ app/src/main/java/com/example/bren/hackdavis/MainActivity.java
@@ -5,21 +5,41 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.EditText;
+import android.content.Intent;
 
-public class MainActivity extends AppCompatActivity{
+public class MainActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         Button submitButton = (Button) findViewById(R.id.submit);
+        final EditText user = (EditText) findViewById(R.id.uName);
+        final EditText pass = (EditText) findViewById(R.id.passphrase);
+        final TextView tv = (TextView)findViewById(R.id.invalid);
+
+        String username = getIntent().getStringExtra("Username");
+        String password = getIntent().getStringExtra("Password");
 
         submitButton.setOnClickListener(
-                new Button.OnClickListener(){
-                    public void onClick(View v){
+            new Button.OnClickListener() {
+                public void onClick(View v) {
+                    if (user.getText().toString().length() > 0 && pass.getText().toString().length() > 0)
                         startActivity(new Intent(MainActivity.this, sidebar.class));
-                    }
+                    else
+                        tv.setVisibility(View.VISIBLE);
                 }
+            }
         );
+    }
+    @Override
+    public void onResume()
+    {  // After a pause OR at startup
+        super.onResume();
+        //Refresh your stuff here
+        TextView tv = (TextView) findViewById(R.id.invalid);
+        tv.setVisibility(View.INVISIBLE);
     }
 }
--- app/src/main/java/com/example/bren/hackdavis/sidebar.java
+++ app/src/main/java/com/example/bren/hackdavis/sidebar.java
@@ -1,5 +1,6 @@
 package com.example.bren.hackdavis;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -12,6 +13,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.*;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class sidebar extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
