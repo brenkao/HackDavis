9e89929a8aad60010bb1fb3f92791e658b19a51b
Michael Tang <miytang@ucsc.edu>
2016-05-07 22:16:19 -0700

    Nav bar updates

--- app/src/main/java/com/example/bren/hackdavis/sidebar.java
+++ app/src/main/java/com/example/bren/hackdavis/sidebar.java
@@ -80,15 +80,13 @@ public class sidebar extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.find_tutor) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.tools) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.history) {
 
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
+        } else if (id == R.id.nav_send) {
 
         } else if (id == R.id.nav_send) {
 
