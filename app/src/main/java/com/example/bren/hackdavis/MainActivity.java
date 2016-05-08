58c1014b7798a9818a9cd8e0df9731c9aba4646c
brenkao <brenkao@gmail.com>
2016-05-07 20:58:35 -0700

    Commit 3

--- app/src/main/java/com/example/bren/hackdavis/MainActivity.java
+++ app/src/main/java/com/example/bren/hackdavis/MainActivity.java
@@ -1,13 +1,25 @@
 package com.example.bren.hackdavis;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button submitButton = (Button) findViewById(R.id.submit);
+
+        submitButton.setOnClickListener(
+                new Button.OnClickListener(){
+                    public void onClick(View v){
+
+                    }
+                }
+        );
     }
 }
